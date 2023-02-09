aa53cfad67b47b6194355bbb8dd456e9d226ed86
Student <vsj2015.us@gmail.com>
2023-02-09 13:45:03 -0800

    initial commit

---
+++ A2_Stucture_in_Memory_and_Buffering/miyazaki_hajime_HW2_main.c
@@ -0,0 +1,12 @@
+/**************************************************************
+* Class:  CSC-415-0# Spring 2023
+* Name:
+* Student ID: Hajime
+* GitHub UserID: Miyazaki
+* Project: Assignment 2 – Stucture in Memory and Buffering
+*
+* File: miyazaki_hajime_HW2_main.c
+*
+* Description:
+*
+**************************************************************/
